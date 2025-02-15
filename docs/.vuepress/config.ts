4889aec915dd7cbc064b3889f2c04b2289c9f1b5
jiweiyeah <yeahjiwei@163.com>
2025-02-15 20:13:00 +0800

    广告代码

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,7 +15,7 @@ export default defineUserConfig({
         ['link', { rel: 'icon', type: 'image/jpg', sizes: '16x16', href: '/images/favicon.jpg' }],
         // ['meta', { name: 'google-site-verification', content: 'X5YSaTDn-pKqQBUKD_05_dQcxVItzEq7Rlbg2ZEU7AM' }],
         ['script', { defer: true, src: 'https://cloud.umami.is/script.js', 'data-website-id': '812ec4a5-ae45-457a-836e-5d32ee958204' }],
-        ['script', { async: true, src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX', crossorigin: 'anonymous' }],
+        ['script', { async: true, src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7611616409438220', crossorigin: 'anonymous' }],
       ],
     theme: plumeTheme({
         hostname: 'https://nextjs.freeourdays.com',
